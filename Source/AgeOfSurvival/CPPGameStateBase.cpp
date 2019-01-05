683d739e5c6c2c8fe03c5cfc1d0d26d647294dbd
JensenJ <jensenbradley@icloud.com>
2019-01-05 23:39:33 +0000

    Time and Date
    Time and Data system added, as well as a BP Game State Class to alter settings to do with time and date such as game speed.

--- Source/AgeOfSurvival/CPPGameStateBase.cpp
+++ Source/AgeOfSurvival/CPPGameStateBase.cpp
@@ -1,4 +1,91 @@
 // Copyright Age of Survival 2019.
 
 #include "CPPGameStateBase.h"
+#include "Math/UnrealMathUtility.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetStringLibrary.h"
+#include "GameFramework/Actor.h"
+#include "GenericPlatform//GenericPlatformMath.h"
 
+ACPPGameStateBase::ACPPGameStateBase() {
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ACPPGameStateBase::BeginPlay() {
+	GameTime.SetNum(3); // Limits size of array to 3
+	GameDate.SetNum(3); // Limits size of array to 3
+
+	Hours = UKismetMathLibrary::Conv_IntToFloat(Hours);
+	Minutes = UKismetMathLibrary::Conv_IntToFloat(Minutes) / 60;
+	Seconds = UKismetMathLibrary::Conv_IntToFloat(Seconds) / 3600;
+	float StartingTime = Hours + Minutes + Seconds;
+	Clockwork = StartingTime;
+
+	GameDate.Insert(Day, 0);
+	GameDate.Insert(Month, 1);
+	GameDate.Insert(Year, 2);
+}
+
+void ACPPGameStateBase::Tick(float DeltaSeconds) {
+	SetClockwork(DeltaSeconds);
+	Clock();
+	Calendar();
+}
+
+void ACPPGameStateBase::SetClockwork(float DeltaSeconds) {
+	float DeltaTimeUnit = DeltaSeconds / TimeUnit * GameSpeedMultiplier;
+	float AddedClockwork = DeltaTimeUnit + Clockwork;
+	float NewDayTick = AddedClockwork / (60 * 24);
+	float Remainder = FGenericPlatformMath::Fmod(AddedClockwork, (60.0f * 24.0f));
+	Clockwork = Remainder;
+	DayTick = NewDayTick;
+}
+
+void ACPPGameStateBase::Clock() {
+	// Seconds
+	float ClockworkSeconds = Clockwork * 3600;
+	float NewSeconds = ClockworkSeconds / 60;
+	float RemainderSeconds = FGenericPlatformMath::Fmod(NewSeconds, 60.0f);
+	UKismetMathLibrary::FFloor(RemainderSeconds);
+	Seconds = RemainderSeconds;
+
+	// Minutes
+	float NewMinutes = NewSeconds / 60;
+	float RemainderMinutes = FGenericPlatformMath::Fmod(NewMinutes, 60.0f);
+	UKismetMathLibrary::FFloor(RemainderMinutes);
+	Minutes = RemainderMinutes;
+
+	// Hours
+	float NewHours = NewMinutes / 60;
+	float RemainderHours = FGenericPlatformMath::Fmod(NewHours, 60.0f);
+	UKismetMathLibrary::FFloor(RemainderHours);
+	Hours = RemainderHours;
+	GameTime.Insert(Seconds, 0);
+	GameTime.Insert(Minutes, 1);
+	GameTime.Insert(Hours, 2);
+
+	FString strHours = FString::FromInt(GameTime[2]);
+	FString HoursMinutesString = UKismetStringLibrary::BuildString_Int(strHours, ":", GameTime[1], "");
+	FString FinalString = UKismetStringLibrary::BuildString_Int(HoursMinutesString, ":", GameTime[0], "");
+	UE_LOG(LogTemp, Warning, TEXT("PEGameStateBase: Time: %s"), *FinalString);
+}
+void ACPPGameStateBase::Calendar() {
+	Day = DayTick + Day;
+	int32 DaysInMonth = UKismetMathLibrary::DaysInMonth(Year, Month);
+	if (Day > DaysInMonth) {
+		Day = 1;
+		Month++;
+	}
+	if (Month > 12) {
+		Month = 1;
+		Year++;
+	}
+	GameDate.Insert(Day, 0);
+	GameDate.Insert(Month, 1);
+	GameDate.Insert(Year, 2);
+
+	FString strDays = FString::FromInt(GameDate[0]);
+	FString DaysMonthString = UKismetStringLibrary::BuildString_Int(strDays, "/", GameDate[1], "");
+	FString FinalString = UKismetStringLibrary::BuildString_Int(DaysMonthString, "/", GameDate[2], "");
+	UE_LOG(LogTemp, Warning, TEXT("PEGameStateBase: Date: %s"), *FinalString);
+}
--- Source/AgeOfSurvival/CPPGameStateBase.h
+++ Source/AgeOfSurvival/CPPGameStateBase.h
@@ -4,14 +4,49 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameStateBase.h"
+#include "CPPPlayerController.h"
 #include "CPPGameStateBase.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class AGEOFSURVIVAL_API ACPPGameStateBase : public AGameStateBase
 {
 	GENERATED_BODY()
 	
+public:
+	ACPPGameStateBase();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar")
+	TArray<int32> GameTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar")
+	TArray<int32> GameDate;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar")
+	float GameSpeedMultiplier;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resources")
+	//int32 Currency
+
+	//UPROPERTY(BlueprintCallable, Category = "Resources")
+	//int32 ChangeCurrency(int32 amount);
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+
+	// Clock Functions
+	void SetClockwork(float DeltaSeconds);
+	void Clock();
+	void Calendar();
+	// Clock Variables
+	float TimeUnit = 0.25f;
+	float Clockwork = 0.0f;
+	float DayTick = 0;
+	int32 Seconds = 0;
+	int32 Minutes = 0;
+	int32 Hours = 0;
+	int32 Day = 1;
+	int32 Month = 1;
+	int32 Year = 1;
+
+
 };
