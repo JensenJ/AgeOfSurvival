3dd2052ca0a591ccc2b52e0cafdd22c6ea2be574
JensenJ <jensenbradley@icloud.com>
2019-01-05 20:46:15 +0000

    Prototyping Level Created
    Prototyping level with basic scene has been created.

---
+++ Source/AgeOfSurvival/AgeOfSurvivalGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AgeOfSurvivalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGEOFSURVIVAL_API AAgeOfSurvivalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
